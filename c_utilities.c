7104b0418e3401b6f5b9e4c5c351d7350800b11f
Faith-M <87698809+Faith-M@users.noreply.github.com>
2023-11-16 20:23:02 +0200

    Update c_utilities.c

--- c_utilities.c
+++ c_utilities.c
@@ -8,10 +8,10 @@
  */
 int is_printable(char c)
 {
-	if (c >= 32 && c < 127)
-		return (1);
+	if (c >= 32 && c < 127)    /* checks if the ASCII value of the character c is greater than or equal to 32 and less than 127*/
+		return (1);   /*If the character c falls within the ASCII range of printable characters (32 to 126 inclusive), the function returns 1, indicating that the character is a printable character*/
 
-	return (0);
+	return (0);   /*If the character c does not fall within this range, the function returns 0, indicating that it is not a printable character.*/
 }
 
 /**
@@ -23,18 +23,18 @@ int is_printable(char c)
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
+	char map_to[] = "0123456789ABCDEF";   /*stores the hexadecimal digits' characters from 0 to F, which will be used for converting the ASCII code to its hexadecimal representation*/
 
 	if (ascii_code < 0)
-		ascii_code *= -1;
+		ascii_code *= -1;    /*ensures that the code works with positive values by converting negative ASCII codes to their positive equivalents*/
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
 	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[i] = map_to[ascii_code % 16];    /*assign the hexadecimal representation of the ASCII code to the buffer*/
 
-	return (3);
+	return (3);   /* indicates that it has converted the ASCII code to its hexadecimal representation and stored it in the buffer, consisting of three characters*/
 }
 
 /**
@@ -45,10 +45,10 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  */
 int is_digit(char c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
+	if (c >= '0' && c <= '9')   /* checks if the character c falls within the ASCII range of numeric characters */
+		return (1);   /*If c is between '0' and '9' (inclusive), the function returns 1, indicating that the character is a numeric digit*/
 
-	return (0);
+	return (0);  /*If c is not in this range, the function returns 0*/
 }
 
 /**
@@ -61,11 +61,11 @@ int is_digit(char c)
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
-		return (num);
+		return (num);   /*If size equals S_LONG, it returns num as is without any type conversion, assuming it's already of the desired type*/
 	else if (size == S_SHORT)
-		return ((short)num);
+		return ((short)num);   /*If size equals S_SHORT, it performs a type cast on num to a short data type and returns the result*/
 
-	return ((int)num);
+	return ((int)num);  /*If neither S_LONG nor S_SHORT is the value of size, it defaults to returning num typecasted as an int*/
 }
 
 /**
