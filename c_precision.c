ae7759090c296c98b148a178570a7b96b7e75083
Faith-M <87698809+Faith-M@users.noreply.github.com>
2023-11-16 20:11:49 +0200

    Update c_precision.c

--- c_precision.c
+++ c_precision.c
@@ -18,21 +18,21 @@ int get_precision(const char *format, int *i, va_list list)
 
 	precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++) /*initializes curr_i to a value and iterates through the format string until it encounters the null terminator '\0'*/
 	{
-		if (is_digit(format[curr_i]))
+		if (is_digit(format[curr_i]))  /*If the current character in the format string is a digit, it updates the precision value*/
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
+			precision *= 10;   /*multiplies the existing precision value by 10 to shift its digits to the left.*/
+			precision += format[curr_i] - '0';   /* adds the numerical value of the character in the format string to the precision value. The subtraction of '0' from format[curr_i] converts the character to its numeric value*/
 		}
-		else if (format[curr_i] == '*')
+		else if (format[curr_i] == '*')     /*If the character is an asterisk, it increments curr_i, expecting the next argument in the variable argument list.*/
 		{
 			curr_i++;
-			precision = va_arg(list, int);
-			break;
+			precision = va_arg(list, int);    /*It retrieves an integer argument from the variable argument list using va_arg(list, int) and assigns it to the precision variable*/
+			break;   /*After this operation, it breaks out of the loop.*/
 		}
 		else
-			break;
+			break;   /**/If neither a digit nor an asterisk is encountered, it breaks out of the loop.
 	}
 
 	*i = curr_i - 1;
