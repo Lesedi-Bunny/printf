3a5024f972129611288e68d4eb9c80d2604fc349
Faith-M <87698809+Faith-M@users.noreply.github.com>
2023-11-16 20:48:04 +0200

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -12,15 +12,15 @@ int get_size(const char *format, int *i)
 	int c_i = *i + 1;
 	int size = 0;
 
-	if (format[c_i] == 'l')
+	if (format[c_i] == 'l') /*checks if the character in the format string at position c_i is 'l'*/
 		size = S_LONG;
 	else if (format[c_i] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = c_i - 1;
+		*i = c_i - 1; /*If size remains 0 after the checks, it sets the value pointed to by i to c_i - 1*/
 	else
-		*i = c_i;
+		*i = c_i; /*Otherwise, it sets i to c_i*/
 
-	return (size);
+	return (size); /*the function returns the value of size, which indicates the size specifier found*/
 }
